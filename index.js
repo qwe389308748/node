db6a343f4a404c85d4afc1c7521625478cc0c504
‘e <389308748@qq.com>
2021-01-31 15:19:06 +0800

    1.全局对象

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log(__dirname)
+
+// console.log("当前命令行：", process.cwd())
+
+// setTimeout(() => {
+//   console.log("abc");
+// }, 1000);
+
+// process.exit();
+
+
+// console.log(process.argv);
+
+// console.log(process.platform);
+
+// console.log(process.env.PROCESSOR_REVISION);
