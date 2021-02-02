6ee4b80b08ae35831faf60bb2487044945a04db4
‘e <389308748@qq.com>
2021-02-02 14:12:48 +0800

    3.基本内置模块

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const os = require('os')
 
-console.log(os.arch())
+// console.log(os.arch())
 
-console.log(os.cpus().length)
+// console.log(os.cpus().length)
 
-console.log(os.freemem()/1024**3)
+// console.log(os.freemem()/1024**3)
+
+// console.log(os.homedir())
+
+// console.log(os.hostname())
+
+console.log(os.tmpdir());
+
---
+++ index.path.js
@@ -0,0 +1,15 @@
+const path =require('path');
+
+// const basename = path.basename("fdg/dfgdfg/adfaf/fdgdfgd/a",'.html')
+// console.log(basename)
+
+// console.log(path.sep)
+
+// console.log(process.env.PATH.split(path.delimiter))
+
+
+// const dir = path.dirname("a/b/c/d")
+// console.log(dir)
+
+// const ext = path.extname("a/b/c/a");
+// console.log(ext);
